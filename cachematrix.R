6d4bf6d9cd66ec879162d6e65916ad36d21ea6fe
JayGoody101 <jay.goodman@arconic.com>
2017-07-28 22:28:45 -0400

    assign2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Pair of functions to invert a matrix and cache the result for subsequent use
+## Using example "mean" function as a template:
+## change vectors to matrices where appropriate
+## change "NULL" setting to "NaN"
+## change evaluate call from Mean to Solve
 
-## Write a short comment describing this function
+## Creates list of functions to store cached version of a matrix inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
+        mInv <- as.matrix(NaN)
+        set <- function(y) {
+                x <<- y
+                mInv <<- as.matrix(NaN)
+        }
+        get <- function() x
+        setInv <- function(pInv) mInv <<- pInv
+        getInv <- function() mInv
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 
 }
 
 
-## Write a short comment describing this function
+## Checks for existence of cached matrix inverse.
+## If present, uses it; if not, inverts input matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+        mInv <- x$getInv()
+        if(!is.nan(mInv[1,1])) {
+                message("getting cached data")
+                return(mInv)
+        }
+        data <- x$get()
+        mInv <- solve(data, ...)
+        mInv <-as.matrix(x$setInv(mInv))
+        mInv
+        
+        }
